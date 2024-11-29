cab067cb548ea9e7f8f5ceaf507dade153b90b8a
maraBOOT <doueameaud13@gmail.com>
2024-11-29 20:44:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //POWERED BY MARABOOT:maraboot225@gmail.com";
-const IFRAME_URL = "https://dev.d15tgskwr500d2.amplifyapp.com/iframe.html";
-const UID = "cnam225_";
+const IFRAME_URL = "https://dev.d15tgskwr500d2.amplifyapp.com/iframe_.html";
+const UID = "cnam225";
 const CONFIG = {
   title: "CNAM",
   logo: ""
